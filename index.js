879fdf9eba84a735d1fc6fdb2da24d8da703f0a3
Scott Everson <148817937+scottie-eves@users.noreply.github.com>
2024-05-23 12:50:28 -0700

    Update index.js
    Main branch updated to properly authenticate movies endpoint

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.get('/users/:Username', async (req, res) => {
 });
 
 //READ 
-app.get('/movies',  async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false}),  async (req, res) => {
   await Movies.find()
   .then((movies) => {
     res.status(201).json(movies);
