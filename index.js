8429fdc21115011c19e3ab4985bfa432652737ed
Scott Everson <148817937+scottie-eves@users.noreply.github.com>
2024-06-26 14:31:30 -0700

    updated allowedOrigins

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://flixnow.netlify.app/'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://flixnow.netlify.app/'];
 
 app.use(cors({
   origin: (origin, callback) => {
