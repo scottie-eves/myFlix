921686f588b240513416b52289a64959227d08d7
Scott Everson <148817937+scottie-eves@users.noreply.github.com>
2024-07-11 09:56:36 -0700

    letting cors allow requests from any domain to do some de-bugging

--- index.js
+++ index.js
@@ -24,16 +24,18 @@ app.use(express.static('public'));
 const cors = require('cors');
 let allowedOrigins = ['Access-Control-Allow-Origin', 'http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://flixnow.netlify.app/', 'https://flix-vault-253ef352783e.herokuapp.com/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 let auth = require('./auth')(app);
 
