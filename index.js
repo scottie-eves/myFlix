89e7194c6ee77fbaff0af0e85585cb3516a9e20d
Scott Everson <148817937+scottie-eves@users.noreply.github.com>
2024-07-11 10:22:43 -0700

    updating cors policy to allow all requests for de-bugging

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-const cors = require('cors');
+// const cors = require('cors');
 // let allowedOrigins = ['Access-Control-Allow-Origin', 'http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://flixnow.netlify.app/', 'https://flix-vault-253ef352783e.herokuapp.com/'];
 
 app.use(cors());
